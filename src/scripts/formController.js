90eabadf2da8e0c6ec80ef8363247c585a9fc9e8
Josh <15219491+jkaps9@users.noreply.github.com>
2026-04-28 22:59:22 -0400

    add freebie

--- src/scripts/formController.js
+++ src/scripts/formController.js
@@ -6,9 +6,17 @@ const headerList = document.querySelectorAll(".form-header");
 const stepNumberList = document.querySelectorAll(
   ".progress-steps__item > .step-number",
 );
+const toggleSwitch = document.querySelector(".switch > input");
+const planPrices = document.querySelectorAll(".plan__price");
+const annualFreebies = document.querySelectorAll(".annual-freebie");
 
 let currentStep = 0;
 const steps = ["one", "two", "three", "four"];
+
+const prices = {
+  monthly: ["$9/mo", "$12/mo", "$15/mo"],
+  yearly: ["$90/yr", "$120/yr", "$150/yr"],
+};
 
 function addClassToListExceptOne(elementList, exception, className) {
   elementList.forEach((e) => {
@@ -60,6 +68,21 @@ function changeSections() {
   );
 }
 
+function updatePrices(key) {
+  const priceValues = key === "yearly" ? prices.yearly : prices.monthly;
+  planPrices.forEach((planPrice, index) => {
+    planPrice.textContent = priceValues[index];
+  });
+
+  annualFreebies.forEach((e) => {
+    if (key === "yearly") {
+      e.classList.remove("hidden");
+    } else {
+      e.classList.add("hidden");
+    }
+  });
+}
+
 nextButton.addEventListener("click", () => {
   // TODO: check form validation first
   currentStep++;
@@ -69,4 +92,12 @@ nextButton.addEventListener("click", () => {
 prevButton.addEventListener("click", () => {
   currentStep--;
   changeSections();
+});
+
+toggleSwitch.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    updatePrices("yearly");
+  } else {
+    updatePrices("monthly");
+  }
 });
